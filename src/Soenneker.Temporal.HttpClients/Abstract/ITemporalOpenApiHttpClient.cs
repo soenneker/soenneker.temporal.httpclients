0b7e4874ce8242704f2e237c1e17d5e7c1fe9f4f
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:52 -0500

    xml commenting

--- src/Soenneker.Temporal.HttpClients/Abstract/ITemporalOpenApiHttpClient.cs
+++ src/Soenneker.Temporal.HttpClients/Abstract/ITemporalOpenApiHttpClient.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Temporal.HttpClients.Abstract;
 /// </summary>
 public interface ITemporalOpenApiHttpClient: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<HttpClient> Get(CancellationToken cancellationToken = default);
 }
